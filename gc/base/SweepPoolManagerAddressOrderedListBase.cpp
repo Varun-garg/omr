9b4e79e46b258925c2d191846c9688b42fd02e8a
Varun Garg <varun.10@live.com>
2017-08-16 17:37:55 +0000

    Removed unneccecarry noaccess call
    Automatically called while freeing objects in valgrind

--- gc/base/SweepPoolManagerAddressOrderedListBase.cpp
+++ gc/base/SweepPoolManagerAddressOrderedListBase.cpp
@@ -543,7 +543,6 @@ MM_SweepPoolManagerAddressOrderedListBase::addFreeMemory(MM_EnvironmentBase *env
 		int objSize = (int) ( (GC_ObjectModel)_extensions->objectModel ).getConsumedSizeInBytesWithHeader( (omrobjectptr_t) *it);
 		omrtty_printf("VALGRIND: Clearing object at %x of size %d\n", *it,objSize);
 		VALGRIND_MEMPOOL_FREE(_extensions->valgrindMempoolAddr,*it);
-		VALGRIND_MAKE_MEM_NOACCESS(*it,objSize);
 	}
 	_extensions->_allocatedObjects.erase(_extensions->_allocatedObjects.lower_bound((uintptr_t)address), 
 		_extensions->_allocatedObjects.upper_bound((uintptr_t)address + (uintptr_t)MM_Bits::convertSlotsToBytes(size)));
